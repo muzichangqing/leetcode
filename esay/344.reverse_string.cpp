5cb8d9e28939ceb6c82a9fc4d8a0c9a71d6915dd
cqli <cqli@i9i8.com>
2018-02-11 16:22:42 +0800

    update 344, add 557

--- esay/344.reverse_string.cpp
+++ esay/344.reverse_string.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string reverseString(string s) {
         size_t size = s.length();
-        string rs(size, ' ');
+        string rs;
         for (int i = 0; i < size; i++) {
-            rs[i] = s[size - 1 - i];
+            rs.push_back(s[size - 1 - i]);
         }
         return rs;
     }

23fc8886792f9d2dce7da3824a2e3dd0f149b86e
cqli <cqli@i9i8.com>
2018-05-23 11:48:34 +0800

    remove test

---
+++ explore/binary-tree/traverse-a-tree/binary-tree-inorder-traversal.c
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    if (root == NULL) {
+        returnSize = 0;
+        return NULL;
+    }
+    
+    const int STACK_ICR_SIZE = 100;
+    const int RESULT_ICR_SIZE = 100;
+
+    struct TreeNode **stack = (struct TreeNode **)malloc(sizeof(struct TreeNode *) * STACK_ICR_SIZE);
+    int *result = (int *)malloc(sizeof(int) * RESULT_ICR_SIZE);
+    int stack_top = 0;
+    *returnSize = 0;
+
+    stack[stack_top++] = root;
+    struct TreeNode *cursorNode;
+
+    while (stack_top > 0) {
+        cursorNode = 
+    }
+
+    free(stack);
+
+    return result;
+}

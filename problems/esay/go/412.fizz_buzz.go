c77ca608b4670a93fbae2d52d6d38deb067d45c8
cqli <cqli@i9i8.com>
2018-10-23 20:27:43 +0800

    格式化

--- problems/esay/go/412.fizz_buzz.go
+++ problems/esay/go/412.fizz_buzz.go
@@ -1,21 +1,21 @@
 package leetcode
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func fizzBuzz(n int) []string {
-    res := make([]string, n)
-    for i := 1; i <= n; i++ {
-        if i % 3 == 0 && i % 5 == 0 {
-            res[i-1] = "FizzBuzz"
-        } else if i % 3 == 0 {
-            res[i-1] = "Fizz"
-        } else if i % 5 == 0 {
-            res[i-1] = "Buzz"
-        } else {
-            res[i-1] = fmt.Sprintf("%d", i)
-        }
-    }
-    return res
+	res := make([]string, n)
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			res[i-1] = "FizzBuzz"
+		} else if i%3 == 0 {
+			res[i-1] = "Fizz"
+		} else if i%5 == 0 {
+			res[i-1] = "Buzz"
+		} else {
+			res[i-1] = fmt.Sprintf("%d", i)
+		}
+	}
+	return res
 }
--- problems/esay/go/669.trim_a_binary_search_tree.go
+++ problems/esay/go/669.trim_a_binary_search_tree.go
@@ -1,22 +1,22 @@
 package leetcode
 
 type TreeNode struct {
-    Val int
-    Left *TreeNode
-    Right *TreeNode
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
 func trimBST(root *TreeNode, L int, R int) *TreeNode {
-    if root == nil {
-        return root
-    }
-    if root.Val < L {
-        return trimBST(root.Right, L, R)
-    }
-    if root.Val > R {
-        return trimBST(root.Left, L, R)
-    }
-    root.Left = trimBST(root.Left, L, R)
-    root.Right = trimBST(root.Right, L, R)
-    return root
+	if root == nil {
+		return root
+	}
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
 }

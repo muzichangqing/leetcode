07a1ce6d08e84284f62ae690e802b04d6bc76dae
cqli <cqli@i9i8.com>
2018-10-23 21:05:24 +0800

    格式化go source file

--- problems/esay/go/496.next_greater_element_1.go
+++ problems/esay/go/496.next_greater_element_1.go
@@ -1,20 +1,20 @@
 package leetcode
 
 func nextGreaterElement(findNums []int, nums []int) []int {
-    res := make([]int, len(findNums))
-    tmp_map := make(map[int]int)
-    nums_size := len(nums)
-    for fi := 0; fi < nums_size; fi++ {
-        tmp_map[nums[fi]] = -1
-        for si := fi + 1; si < nums_size; si++ {
-            if nums[si] > nums[fi] {
-                tmp_map[nums[fi]] = nums[si]
-                break
-            }
-        }
-    }
-    for index, value := range(findNums) {
-        res[index] = tmp_map[value]
-    }
-    return res
+	res := make([]int, len(findNums))
+	tmp_map := make(map[int]int)
+	nums_size := len(nums)
+	for fi := 0; fi < nums_size; fi++ {
+		tmp_map[nums[fi]] = -1
+		for si := fi + 1; si < nums_size; si++ {
+			if nums[si] > nums[fi] {
+				tmp_map[nums[fi]] = nums[si]
+				break
+			}
+		}
+	}
+	for index, value := range findNums {
+		res[index] = tmp_map[value]
+	}
+	return res
 }

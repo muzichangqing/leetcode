4f10d06102d660aca2ce6bbafa28fa87d14a277d
cqli <cqli@i9i8.com>
2018-10-25 22:10:29 +0800

    格式化

--- explore/binary-tree/solve-tree-problems-recursively/symmetric-tree.cpp
+++ explore/binary-tree/solve-tree-problems-recursively/symmetric-tree.cpp
@@ -10,46 +10,46 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-    	vector<TreeNode *> *traverse_before_reverse = new vector<TreeNode *>;
-    	vector<TreeNode *> *traverse_after_reverse = new vector<TreeNode *>;
-    	traverseTree(root, traverse_before_reverse);
-    	reverseTree(root);
-    	traverseTree(root, traverse_after_reverse);
-    	if (traverse_before_reverse->size() != traverse_after_reverse->size()) {
-    		return false;
-    	}
-    	for(int i = 0; i < traverse_after_reverse->size(); i++) {
-			TreeNode *node1 = traverse_before_reverse->at(i);
-			TreeNode *node2 = traverse_after_reverse->at(i);
-			if (node1 == NULL && node2 != NULL) {
-				return false;
-			}
-			if (node1 != NULL && node2 == NULL) {
-				return false;
-			}
-			if (node1 != NULL && node1->val != node2->val) {
-				return false;
-			}
-    	}
-    	delete traverse_before_reverse;
-    	delete traverse_after_reverse;
-    	return true;
+        vector<TreeNode *> *traverse_before_reverse = new vector<TreeNode *>;
+        vector<TreeNode *> *traverse_after_reverse = new vector<TreeNode *>;
+        traverseTree(root, traverse_before_reverse);
+        reverseTree(root);
+        traverseTree(root, traverse_after_reverse);
+        if (traverse_before_reverse->size() != traverse_after_reverse->size()) {
+            return false;
+        }
+        for(int i = 0; i < traverse_after_reverse->size(); i++) {
+            TreeNode *node1 = traverse_before_reverse->at(i);
+            TreeNode *node2 = traverse_after_reverse->at(i);
+            if (node1 == NULL && node2 != NULL) {
+                return false;
+            }
+            if (node1 != NULL && node2 == NULL) {
+                return false;
+            }
+            if (node1 != NULL && node1->val != node2->val) {
+                return false;
+            }
+        }
+        delete traverse_before_reverse;
+        delete traverse_after_reverse;
+        return true;
     }
 
     TreeNode *reverseTree(TreeNode *root) {
-    	if (root == NULL) return NULL;
-		TreeNode *tmp = root->right;
-    	root->right = reverseTree(root->left);
-    	root->left = reverseTree(tmp);
-    	return root;
+        if (root == NULL) return NULL;
+	    TreeNode *tmp = root->right;
+        root->right = reverseTree(root->left);
+        root->left = reverseTree(tmp);
+        return root;
     }
 
     void traverseTree(TreeNode *root, vector<TreeNode *> *result) {
-		result->push_back(root);
-    	if (root != NULL) {
-			traverseTree(root->left, result);
-    		traverseTree(root->right, result);
-		}
-    	return;
+        result->push_back(root);
+        if (root != NULL) {
+            traverseTree(root->left, result);
+            traverseTree(root->right, result);
+        }
+        return;
     }
 };

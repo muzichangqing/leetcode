cad6fef49d26931f817beb5731ebcb03dfd4c859
cqli <cqli@i9i8.com>
2018-10-26 11:48:09 +0800

    path sum

---
+++ explore/binary-tree/solve-tree-problems-recursively/path_sum.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        int currentSum = 0;
+        return hasPathSumRecursively(root, currentSum, sum);   
+    }
+
+    bool hasPathSumRecursively(TreeNode *root, int currentSum, int sum) {
+        if (root == NULL) {
+            return false;
+        }
+
+        currentSum += root->val;
+        if (currentSum == sum && root->left == NULL && root->right == NULL) {
+            return true;
+        }
+        return hasPathSumRecursively(root->left, currentSum, sum) 
+            || hasPathSumRecursively(root->right, currentSum, sum);
+    }
+};

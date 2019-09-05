48af45d2a54c7ee2cac9aca71ff33e214e49d962
muzichangqing <code_lcq@163.com>
2019-09-05 19:51:24 +0800

    add valid_parentheses solution

---
+++ explore/queue&stack/stack/valid_parentheses.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+
+int isValid(char * s){
+    int strSize = strlen(s);
+    size_t strMemSize = strSize * sizeof(char);
+    if (strMemSize == 0) return 1;
+    char* stack = (char*)malloc(strMemSize);
+    int top = -1;
+    for(int i = 0; i < strSize; i++) {
+        char c = s[i];
+        if (top != -1 && stack[top] != c && abs(c - stack[top]) < 3) top--;
+        else stack[++top] = c;
+    }
+    return top == -1;
+}

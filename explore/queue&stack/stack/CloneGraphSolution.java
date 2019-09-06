3804963b667a403885a358311a97a5a2bf561642
muzichangqing <code_lcq@163.com>
2019-09-06 16:20:14 +0800

    修改目录结构

---
+++ explore/queue&stack/stack/CloneGraphSolution.java
@@ -0,0 +1,19 @@
+public class CloneGraphSolution {
+    public Node cloneGraph(Node node) {
+        
+        return null;
+    }
+}
+
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};

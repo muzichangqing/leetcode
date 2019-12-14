ab83b538acb2b045f44e15bd10c0fd5aa46eef21
muzichangqing <code_lcq@163.com>
2019-12-14 15:38:28 +0800

    c语言int溢出处理

---
+++ problems/10.regular_expression_matching.c
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+
+bool isMatch(char * s, char * p){
+
+}
+
--- problems/9.palindrome_number.c
+++ problems/9.palindrome_number.c
@@ -6,8 +6,13 @@ bool isPalindrome(int x) {
     int y = 0;
     int tmp = x;
     while (x != 0) {
+         // 判断是否越界
+        if (y == 214748364 && x % 10 > 7 || y > 214748364) {
+            return false;
+        }
         y = y * 10 + x % 10;
         x /= 10;
+
     }
     if (y == tmp) return true;
     else return false;

611abea78618bca456e45e381a09f9d8f774c2c7
muzichangqing <code_lcq@163.com>
2019-12-21 16:51:11 +0800

    pivot index c

--- explore/array_and_string/pivot_index.c
+++ explore/array_and_string/pivot_index.c
@@ -1,4 +1,20 @@
 int pivotIndex(int* nums, int numsSize){
-
+    if (numsSize == 0) {
+        return -1;
+    }
+    int sumR = 0;
+    for (int i = 0; i < numsSize; i++) {
+        sumR += nums[i];
+    }
+    int sumL = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (i > 0) {
+            sumL += nums[i-1];
+            sumR -= nums[i-1];
+        }
+        if (sumL == sumR - nums[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
-

f9c27862a8586af6a40bde0a9d2c885bc67ed54d
muzichangqing <code_lcq@163.com>
2020-07-06 16:21:40 +0800

    63.unique_path_with_obstacles

--- problems/golang/63.unique_path_with_obstacles.go
+++ problems/golang/63.unique_path_with_obstacles.go
@@ -1,18 +1,30 @@
 package leetcode
 
-type robot struct {
-	x, y int
-}
-
-func (r *robot) toRight(obstacleGrid [][]int) bool {
-	return false
-}
-
-func (r *robot) toDown(obstacleGrid [][]int) bool {
-	return false
-}
-
 func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	if obstacleGrid == nil || len(obstacleGrid) == 0 || len(obstacleGrid[0]) == 0 {
+		return 0
+	}
+	rowSize := len(obstacleGrid)
+	colSize := len(obstacleGrid[0])
+	dp := make([][]int, rowSize)
+	for i := range dp {
+		dp[i] = make([]int, colSize)
+	}
 
-	return 0
+	for i := 0; i < rowSize && obstacleGrid[i][0] == 0; i++ {
+		dp[i][0] = 1
+	}
+	for j := 0; j < colSize && obstacleGrid[0][j] == 0; j++ {
+		dp[0][j] = 1
+	}
+
+	for i := 1; i < rowSize; i++ {
+		for j := 1; j < colSize; j++ {
+			if obstacleGrid[i][j] == 1 {
+				continue
+			}
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[rowSize-1][colSize-1]
 }

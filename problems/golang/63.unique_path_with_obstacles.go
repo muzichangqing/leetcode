f401045948d13ce4b2daac92fad421c8792c578e
muzichangqing <code_lcq@163.com>
2020-07-06 10:41:19 +0800

    修改golang的目录结构

---
+++ problems/golang/63.unique_path_with_obstacles.go
@@ -0,0 +1,18 @@
+package leetcode
+
+type robot struct {
+	x, y int
+}
+
+func (r *robot) toRight(obstacleGrid [][]int) bool {
+	return false
+}
+
+func (r *robot) toDown(obstacleGrid [][]int) bool {
+	return false
+}
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+
+	return 0
+}

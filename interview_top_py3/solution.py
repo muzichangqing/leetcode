365d1cf0f73dacb25c3c67e56fde026d863bee90
muzichangqing <code_lcq@163.com>
2023-01-29 13:14:01 +0800

    add interview_top_py3

---
+++ interview_top_py3/solution.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self,
+                      list1: Optional[ListNode],
+                      list2: Optional[ListNode]) -> Optional[ListNode]:
+        if list1 is None:
+            return list2
+        if list2 is None:
+            return list1
+        head = ListNode()
+        cursor = head
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                cursor.next = list1
+                list1 = list1.next
+            else:
+                cursor.next = list2
+                list2 = list2.next
+            cursor = cursor.next
+        if list1 is not None:
+            cursor.next = list1
+        else:
+            cursor.next = list2
+        return head.next

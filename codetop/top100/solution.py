35b0097842c9c184da963320ae94bf00cea00ea5
muzichangqing <code_lcq@163.com>
2023-02-16 20:24:15 +0800

    2023.2.16.2

--- codetop/top100/solution.py
+++ codetop/top100/solution.py
@@ -16,9 +16,40 @@ class Solution:
             return mini if mini < int(1e9) else -1
         if amount < 1: return 0
         return dp(amount)
+    
+    def numberOfPairs(self, nums: List[int]) -> List[int]:
+        count = 0
+        m = set([])
+        for num in nums:
+            if num in m:
+                m.remove(num)
+                count += 1
+            else:
+                m.add(num)
+        return [count, len(nums)-count*2]
+    
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range((n+1)>>1):
+            for j in range(i, n-1-i):
+                i2 = n-i-1
+                j2 = n-j-1
+                matrix[i][j], matrix[j][i2], matrix[i2][j2], matrix[j2][i] = \
+                matrix[j2][i], matrix[i][j], matrix[j][i2], matrix[i2][j2]
+
 
 if __name__ == '__main__':
     s = Solution()
     coins = [1, 2, 5]
     amount = 11
     print(s.coinChange(coins, amount))
+
+    nums = [1,3,2,1,3,2,2]
+    print(s.numberOfPairs(nums))
+
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    s.rotate(matrix)
+    print(matrix)

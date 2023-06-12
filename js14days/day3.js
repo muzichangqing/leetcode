39949e9020ee81aa23e3688b730d8482bdf9c81a
code_lcq <code_lcq@163.com>
2023-06-12 20:51:05 +0800

    20230612_1483 && jsday4

---
+++ js14days/day3.js
@@ -0,0 +1,33 @@
+// 2634. 过滤数组中的元素
+
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {Function} fn 
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            res.push(arr[i]);
+        }
+    }
+    return res;
+};
+
+// 2626. 数组归纳运算
+
+/**
+ * 
+ * @param {number[]} nums 
+ * @param {Function} fn 
+ * @param {number} init 
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    for (let i = 0; i < nums.length; i++) {
+        init = fn(init, nums[i])
+    }
+    return init
+};

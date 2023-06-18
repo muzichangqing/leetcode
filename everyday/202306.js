f86e9c93f387ff2837821fb2c46acca900cb11d6
muzichangqing <code_lcq@163.com>
2023-06-18 15:42:58 +0800

    20230618

--- everyday/202306.js
+++ everyday/202306.js
@@ -1,4 +1,4 @@
-// 分割圆的最少切割次数
+// 2481. 分割圆的最少切割次数
 /**
  * 
  * @param {number} n
@@ -12,4 +12,40 @@ var numberOfCuts = function(n) {
         return n / 2;
     }
     return n;
+};
+/**
+ * 1254. 统计封闭岛屿的数目
+ * @param {number[][]} grid
+ * @return {number} 
+ */
+var closedIsland = function(grid) {
+    let height = grid.length;
+    let width = grid[0].length;
+    let dfs = function(i, j) {
+        if (i === -1 || i === height || j === -1 || j === width) {
+            return false;
+        }
+        if (grid[i][j] !== 0) {
+            return true;
+        }
+        grid[i][j] = 2;
+        // 避免短路，一个一个执行，而不是 bfs(i-1,j) && dfs(i+1, j) && ...
+        let up = dfs(i-1, j);
+        let down = dfs(i+1, j);
+        let left = dfs(i, j-1);
+        let right = dfs(i, j+1);
+        return up && down && left && right;
+    };
+    let count = 0;
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < width; j++) {
+            if (grid[i][j] !== 0) {
+                continue;
+            }
+            if (dfs(i, j)) {
+                count++;
+            }
+        }
+    }
+    return count;
 };
